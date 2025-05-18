1c510e002680b9104174bf777977f3a1e55f5c8b
Shilpa Mukhopadhyay <shilpamukhopadhyayms@gmail.com>
2025-05-17 17:02:02 -0700

    channel selection 100

--- opv2v/opencood/models/corpbevt.py
+++ opv2v/opencood/models/corpbevt.py
@@ -131,10 +131,13 @@ class CorpBEVT(nn.Module):
         # x = x.squeeze(1)
 
         #shilpa new fax - ca + egosend + cav reconstruction phase 1
-        if self.prev_avg_entropy is not None:
-            orig_bev_data_from_all_cav, selected_indices = self.fax(batch_dict, self.prev_avg_entropy)
-        else:
-            orig_bev_data_from_all_cav, selected_indices = self.fax(batch_dict)
+        #shilpa channel entropy
+        # if self.prev_avg_entropy is not None:
+        #     orig_bev_data_from_all_cav, selected_indices = self.fax(batch_dict, self.prev_avg_entropy)
+        # else:
+        #     orig_bev_data_from_all_cav, selected_indices = self.fax(batch_dict)
+        orig_bev_data_from_all_cav, selected_indices = self.fax(batch_dict)
+
         x = orig_bev_data_from_all_cav
 
         x, _ = regroup(x, record_len, self.max_cav)
@@ -145,12 +148,18 @@ class CorpBEVT(nn.Module):
         max_cav = x.shape[1]  # max_cav = 5 (from x.shape)
         batch_size = x.shape[0]
 
-        x = x.reshape(batch_size, max_cav, c, -1)
+        #shilpa channel entropy
+        # x = x.reshape(batch_size, max_cav, c, -1)
+        # selected_output_values = torch.zeros(batch_size, max_cav, c, selected_indices.shape[0], device=x.device) 
+        # for idx, value in enumerate(selected_indices):
+        #         # Use advanced indexing to copy values
+        #         selected_output_values[:, :, :, idx] = x[:, :, :, value].clone()
 
-        selected_output_values = torch.zeros(batch_size, max_cav, c, selected_indices.shape[0], device=x.device) 
+
+        selected_output_values = torch.zeros(batch_size, max_cav, selected_indices.shape[0], h, w, device=x.device) 
         for idx, value in enumerate(selected_indices):
                 # Use advanced indexing to copy values
-                selected_output_values[:, :, :, idx] = x[:, :, :, value].clone()
+                selected_output_values[:, :, idx, :,:] = x[:, :, value, :,:].clone()
 
         #response : selected_output_values
       
@@ -176,22 +185,36 @@ class CorpBEVT(nn.Module):
         
         # Step 3: Replace values at locations indicated by select_indices
         # Step 1: Reshape replicated_data to [1, k, 128, 1024]
-        replicated_data_flat = replicated_data.reshape(batch_size, n, c, h * w)
+
+        #shilpa channel entropy comment
+        # replicated_data_flat = replicated_data.reshape(batch_size, n, c, h * w)
 
         # Step 2: Slice selected_output_values to only consider the first k slices
         selected_output_values_k = selected_output_values[:, :n, :, :]  # Shape: [1, k, 128, 307]
 
         # Step 3: Expand selected_indices to match the shape of selected_output_values_k
-        selected_indices_expanded = selected_indices.unsqueeze(0).unsqueeze(0).unsqueeze(2)  # Shape: [1, 1, 1, 307]
-        selected_indices_expanded = selected_indices_expanded.expand(batch_size, n, c, selected_output_values.shape[3])  # Shape: [1, k, 128, 307]
+        #shilpa channel entropy
+        # selected_indices_expanded = selected_indices.unsqueeze(0).unsqueeze(0).unsqueeze(2)  # Shape: [1, 1, 1, 307]
+        # selected_indices_expanded = selected_indices_expanded.expand(batch_size, n, c, selected_output_values.shape[3])  # Shape: [1, k, 128, 307]
+        # selected_indices_expanded = selected_indices.unsqueeze(0).unsqueeze(0)
+        # selected_indices_expanded = selected_indices_expanded.unsqueeze(-1).unsqueeze(-1)
+        # selected_indices_expanded = selected_indices_expanded.expand(1, n, 128, h, w)
 
+        
+        #shilpa channel entropy
         # Step 4: Replace values in replicated_data_flat using scatter_
-        replicated_data_flat_clone = replicated_data_flat.clone()  # Create a copy of the tensor
-        replicated_data_flat_clone.scatter_(3, selected_indices_expanded, selected_output_values_k)
+        # replicated_data_flat_clone = replicated_data_flat.clone()  # Create a copy of the tensor
+        # replicated_data_flat_clone.scatter_(3, selected_indices_expanded, selected_output_values_k)
+        # # Step 5: Reshape replicated_data_flat back to [k, 128, 32, 32]
+        # replicated_data = replicated_data_flat_clone.view(n, c, h, w)
+        
+        replicated_data = replicated_data.clone()
+        replicated_data[:, :, selected_indices, :, :] = selected_output_values_k[:, :, :len(selected_indices), :, :]
+        replicated_data=replicated_data.squeeze(0)
 
 
-        # Step 5: Reshape replicated_data_flat back to [k, 128, 32, 32]
-        replicated_data = replicated_data_flat_clone.view(n, c, h, w)
+
+        
         # print("device of replocated_data=", replicated_data.device)
         #shilpa transform sa fix
         x = replicated_data
@@ -241,62 +264,45 @@ class CorpBEVT(nn.Module):
         b = x.shape[0]
         output_dict = self.seg_head(x, b, 1)
 
-        #shilpa entropy
-        # Normalize the tensor to get probabilities
-        if self.target == 'static':
-            target_tensor = output_dict['static_seg'].detach().cpu().numpy()
-        else:
-            target_tensor = output_dict['dynamic_seg'].detach().cpu().numpy()
+        #shilpa channel entropy
+        # #shilpa entropy
+        # # Normalize the tensor to get probabilities
+        # if self.target == 'static':
+        #     target_tensor = output_dict['static_seg'].detach().cpu().numpy()
+        # else:
+        #     target_tensor = output_dict['dynamic_seg'].detach().cpu().numpy()
             
-        # probabilities = target_tensor / target_tensor.sum(axis=2, keepdims=True)
+        # # probabilities = target_tensor / target_tensor.sum(axis=2, keepdims=True)
+        # # # Add a small epsilon to avoid log(0)
+        # # epsilon = 1e-10
+        # # probabilities += epsilon
+
+        # # Convert logits to probabilities using softmax
+        # exp_logits = np.exp(target_tensor - np.max(target_tensor, axis=2, keepdims=True))  # Stabilize for numerical safety
+        # probabilities = exp_logits / exp_logits.sum(axis=2, keepdims=True)
+
         # # Add a small epsilon to avoid log(0)
         # epsilon = 1e-10
-        # probabilities += epsilon
+        # probabilities = np.clip(probabilities, a_min=epsilon, a_max=None)  # Ensure probabilities are valid
 
-        # Convert logits to probabilities using softmax
-        exp_logits = np.exp(target_tensor - np.max(target_tensor, axis=2, keepdims=True))  # Stabilize for numerical safety
-        probabilities = exp_logits / exp_logits.sum(axis=2, keepdims=True)
+        # # Calculate entropy for each pixel using vectorized operations
+        # entropy_map = entropy(probabilities, axis=2, base=2)
+        # # Reshape and calculate average entropy over 32x32 buckets
+        # entropy_map = entropy_map.squeeze()
+        # bucket_size = 8
+        # entropy_map_reshaped = entropy_map.reshape(256 // bucket_size, bucket_size, 256 // bucket_size, bucket_size)
+        # # entropy_map_reshaped = entropy_map.reshape(bucket_size, 256//bucket_size, bucket_size, 256//bucket_size)
+        # avg_entropy = entropy_map_reshaped.mean(axis=(1, 3))
+        # #shilpa soft entropy
+        # # Normalize avg_entropy between 0 and 1
+        # min_entropy = avg_entropy.min()
+        # max_entropy = avg_entropy.max()
+        # normalized_avg_entropy = (avg_entropy - min_entropy) / (max_entropy - min_entropy)
 
-        # Add a small epsilon to avoid log(0)
-        epsilon = 1e-10
-        probabilities = np.clip(probabilities, a_min=epsilon, a_max=None)  # Ensure probabilities are valid
-
-        # Calculate entropy for each pixel using vectorized operations
-        entropy_map = entropy(probabilities, axis=2, base=2)
-        # Reshape and calculate average entropy over 32x32 buckets
-        entropy_map = entropy_map.squeeze()
-        bucket_size = 8
-        entropy_map_reshaped = entropy_map.reshape(256 // bucket_size, bucket_size, 256 // bucket_size, bucket_size)
-        # entropy_map_reshaped = entropy_map.reshape(bucket_size, 256//bucket_size, bucket_size, 256//bucket_size)
-        avg_entropy = entropy_map_reshaped.mean(axis=(1, 3))
-        #shilpa soft entropy
-        # Normalize avg_entropy between 0 and 1
-        min_entropy = avg_entropy.min()
-        max_entropy = avg_entropy.max()
-        normalized_avg_entropy = (avg_entropy - min_entropy) / (max_entropy - min_entropy)
-
-        # Reshape to desired output shape
-        # self.prev_avg_entropy = torch.tensor(avg_entropy.reshape(1, 1, 1, 32, 32))
-        self.prev_avg_entropy = torch.tensor(normalized_avg_entropy.reshape(1, 1, 1, 32, 32))
+        # # Reshape to desired output shape
+        # # self.prev_avg_entropy = torch.tensor(avg_entropy.reshape(1, 1, 1, 32, 32))
+        # self.prev_avg_entropy = torch.tensor(normalized_avg_entropy.reshape(1, 1, 1, 32, 32))
 
 
-
-        # #shilpa bev dim match
-        # curr_available_bev = output_dict
-        
-        # # Here, selecting the first channel (index 0)
-        # output = output_dict['static_seg'].squeeze(0).squeeze(0)
-
-        # # If you need to resize one specific channel, select it like this:
-        # output_channel = output[0]  # Select the first channel
-
-        # # Ensure the tensor is 4D: [N, C, H, W]
-        # output_channel = output_channel.unsqueeze(0).unsqueeze(0)
-
-        # # Resize to [256, 256]
-        # output_dict['static_seg'] = F.interpolate(output_channel, size=(256, 256), mode='nearest').squeeze(0)
-
-        # # Now output_resized should have the shape [256, 256]
-        # # print(output_dict.shape)  # Output: torch.Size([256, 256])
 
         return output_dict
